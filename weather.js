6a966caf01a76cdde036129dcf2de507f32ea30d
Subrajyoti Saha <80414005+Subro1999@users.noreply.github.com>
2021-05-13 15:02:36 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -4,8 +4,11 @@ var city=document.querySelector('#cityoutput');
 var des=document.querySelector('#description');
 var temp_2=document.querySelector('#temp_feel')
 var temp=document.querySelector('#temp');
+var temp_max=document.querySelector("#temp_max");
+var temp_min=document.querySelector('#temp_min');
 var wind=document.querySelector('#wind');
-var ic=document.querySelector('#icon');
+var temp_1;
+/*var ic=document.querySelector('#icon');*/
 
 apik = "23d5bf948f2c23745e047cfb3259b054";
 
@@ -26,18 +29,21 @@ btn.addEventListener('click',function(){
         var descrip="Climate: " + data['weather']['0']['description']
         var tempera=data['main']['feels_like']
         var temper=data['main']['temp']
+        var tem_max=data['main']['temp_max']
+        var tem_min=data['main']['temp_min']
         var wndspd="Wind speed: " + data['wind']['speed']
-        /*var iconval=data['weather']['0']['icon']*/
+    
 
         city.innerText=nameval;
         des.innerText=descrip;
-        temp_2.innerText=con(tempera);
-        temp.innerText=convertion(temper);
+        temp_2.innerText="Feels like: " + con(tempera);
+        temp.innerText="Actually: " + convertion(temper);
+        temp_max.innerText="Max Temperature: " + con(tem_max);
+        temp_min.innerText="Min Temperature: " + con(tem_min);
         wind.innerText=wndspd;
-        /*iconic.innerText=iconval;*/
+        
     })
     
     
     .catch(err=>alert("Wrong City"))
 });
-/*fetch(api.openweathermap.org/data/2.5/weather?q=India,uk&callback=test&appid={"23d5bf948f2c23745e047cfb3259b054"})*/
